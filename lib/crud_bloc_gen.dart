e5f521acaa8cde0fcb036aa70222011e3ee8be99
Ardi W <braint@masamune.local>
2025-02-08 12:59:38 +0700

    code generator for flutter

--- lib/crud_bloc_gen.dart
+++ lib/crud_bloc_gen.dart
@@ -1,3 +1,10 @@
 library crud_bloc_gen;
 
 export 'src/annotations.dart';
+export 'src/generators/bloc_generator.dart';
+export 'src/generators/datasource_generator.dart';
+export 'src/generators/model_generator.dart';
+export 'src/generators/repository_generator.dart';
+export 'src/generators/usecase_generator.dart';
+export 'src/generators/view_generator.dart';
+export 'src/builder.dart';
